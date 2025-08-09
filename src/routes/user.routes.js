168c666e5137030be21d3f0fcd16c4d83f8df9d0
Animatedguess <105106737+Animatedguess@users.noreply.github.com>
2025-08-09 18:36:40 +0530

    removed unnecessary code from user controller and route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,33 +1,12 @@
 import { Router } from "express";
 import { requireAuthMiddleware } from "../middlewares/clerkAuthMiddleware.js";
-import { createUser, defaultCreateUser, getUser, updateDetails, updateUser, userData } from "../controllers/user.controller.js";
+import { updateUser } from "../controllers/user.controller.js";
 import { dataBaseAuthMiddleware } from "../middlewares/dataBaseAuthMiddleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.route("/skip").post(requireAuthMiddleware, defaultCreateUser);
-router.route("/create").post(requireAuthMiddleware, upload.fields([
-    {
-        name: "avatar",
-        maxCount: 1
-    }
-]), createUser);
-router.route("/data").get(requireAuthMiddleware, dataBaseAuthMiddleware, getUser);
-router.route("/update").post(
-    requireAuthMiddleware, 
-    dataBaseAuthMiddleware,
-    upload.fields([
-        {
-            name: "avatar",
-            maxCount: 1
-        }
-    ]),
-    updateDetails
-);
-
-router.route("/get").get(userData);
 router.route("/update").patch(verifyJWT, updateUser);
 
 export default router;
